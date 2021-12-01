ba89bda7a3dc07ef764f263d5be5e1ec8d33d029
rl27 <rl27@princeton.edu>
2021-12-01 01:18:33 -0500

    Remove unnecessary include

--- HyperViz/Camera.h
+++ HyperViz/Camera.h
@@ -4,7 +4,6 @@
 #include <glad/glad.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
-#include "hyper.h"
 
 // Defines several possible options for camera movement. Used as abstraction to stay away from window-system specific input methods
 enum Camera_Movement {
